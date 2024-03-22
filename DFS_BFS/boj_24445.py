98a739678431b9610b4d818a8c9cb9a780404b42
Jinyoung Lee <didi668@naver.com>
2024-03-23 01:04:03 +0900

    new problems added

---
+++ DFS_BFS/boj_24445.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+numNode, numEdge, startPoint = list(map(int, input().split()))
+
+graph = [[] for _ in range(numNode + 1)]
+visited = [0 for _ in range(numNode + 1)]
+counter = 0
+
+for i in range(numEdge):
+    src, dst = list(map(int, input().split()))
+    
+    graph[src].append(dst)
+    graph[dst].append(src)
+    
+for i in range(1, numNode + 1):
+    graph[i].sort(reverse=True)
+    
+def bfs(st):
+    global counter
+    
+    queue = deque()
+    queue.append(graph[st])
+    counter += 1
+    visited[st] = counter
+    
+    while queue:
+        nextNode = queue.popleft()
+        
+        for adj in nextNode:
+            if visited[adj] == 0:
+                queue.append(graph[adj])
+                counter += 1
+                visited[adj] = counter
+                
+bfs(startPoint)
+
+for i in range(1, numNode + 1):
+    print(visited[i])
