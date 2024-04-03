8bc0e42336fb0ab4edc41ebcdd08976e2b665e91
Jinyoung Lee <didi668@naver.com>
2024-04-03 21:27:02 +0900

    new problems added

---
+++ sort/boj_1181.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+lst = list()
+for i in range(N):
+    lst.append(input())
+    
+setList = set(lst)
+lst = list(setList)
+
+lst.sort()
+lst.sort(key=lambda x: len(x))
+
+
+for i in range(len(lst)):
+    print(lst[i])
