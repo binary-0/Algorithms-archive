fca6e6f8fbc7108b59f68add8ea24f891295930f
Jinyoung Lee <didi668@naver.com>
2024-04-04 23:56:23 +0900

    new problems added

--- String/leetcode_Alg9.py
+++ String/leetcode_Alg9.py
@@ -1,4 +1,31 @@
-intStr = input()
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        isNeg = False
+        strLen = 0
 
+        if x < 0:
+            return False    
 
-for i in range(0, (len(intStr) - 1) // 2)
+        y = x * 1
+
+        while x > 0:
+            strLen += 1
+            x = x // 10
+
+        m = 1
+        stack = []
+        for i in range(0, strLen // 2):
+            stack.append(y % 10)
+            y = y // 10
+
+        if strLen % 2 == 1:
+            y = y // 10
+
+        for i in range(0, strLen // 2):
+            a = stack.pop()
+            if a != y % 10:
+                return False
+            y = y // 10
+        
+        return True
+
